e047562bb0ffd26beb19a46e7bfd7b0d048bd02f
ts.jia <gl123456>
2019-01-26 15:41:34 +0800

    自动备份脚本

--- src/main/shell/sh/autobak.sh
+++ src/main/shell/sh/autobak.sh
@@ -4,11 +4,11 @@
 # 等价于date=$(date "+%y%m%d")
 date=$(date +\%y\%m\%d_\%H\%M\%S)
 size=$(du -sh /etc)
-# mkdir: cannot create directory '/tmp': Permission denied
-# $(mkdir -p /tmp/dbbak)
-mkdir -p /tmp/dbbak
-echo "Date: $date" > /tmp/dbbak/dbinfo.txt
-echo "Data size: $size" >> /tmp/dbbak/dbinfo.txt
-cd /tmp/dbbak
-tar -zcf etc_$date.tar.gz /etc /tmp/dbbak/dbinfo.txt &> /dev/null
-rm -rf /tmp/dbbak/dbinfo.txt
+# Idea  mkdir: cannot create directory '/tmp': Permission denied
+# $(mkdir -p ~/tmp/dbbak)
+mkdir -p ~/tmp/dbbak
+echo "Date: $date" > ~/tmp/dbbak/dbinfo.txt
+echo "Data size: $size" >> ~/tmp/dbbak/dbinfo.txt
+cd ~/tmp/dbbak
+tar -zcf etc_$date.tar.gz /etc ~/tmp/dbbak/dbinfo.txt &> /dev/null
+rm -rf ~/tmp/dbbak/dbinfo.txt
