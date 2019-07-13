71d4f966d5532d9fc89149f1a12b2d47767e7686
ts.jia <gl123456>
2019-07-13 16:31:49 +0800

    for循环

---
+++ src/main/shell/sh/for1-1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# 打印时间
+# Author: TosinJia
+
+for item in morning noon afternooo ; do
+    echo "The time is $item."
+done
+
+
---
+++ src/main/shell/sh/for2-1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# 从1加到100
+# Author: TosinJia
+
+sum=0
+for (( i = 1; i <= 100; i=i+1 )); do
+    sum=$(( $sum + i ))
+done
+echo "The sum is $sum"
+
